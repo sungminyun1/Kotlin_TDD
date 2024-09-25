9d78d77f2c0df9b2d966486cf5081a13ab890bfc
sungminyun1 <smy07207@gmail.com>
2024-09-25 22:54:56 +0900

    fix: remove SpringBootTest annotation
    Remove SpringBootTest annotation

--- src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class PointServiceTest {
 
     private lateinit var userPointTable: UserPointTable
