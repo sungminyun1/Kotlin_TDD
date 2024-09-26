ee27c227fb624e5f5936187dcb83524edfd01bbf
sungminyun1 <smy07207@gmail.com>
2024-09-26 20:43:15 +0900

    feat: handle concurrent request
    Handle concurrent request in service layer
    
    Closes #9

--- src/main/kotlin/io/hhplus/tdd/service/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/service/PointService.kt
@@ -7,52 +7,115 @@ import io.hhplus.tdd.point.TransactionType
 import io.hhplus.tdd.point.UserPoint
 import io.hhplus.tdd.point.dto.response.PointHistoryResponse
 import io.hhplus.tdd.point.dto.response.UserPointResponse
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
 
 @Service
 class PointService @Autowired constructor(
     private val pointHistoryTable: PointHistoryTable,
     private val userPointTable: UserPointTable
 ){
+    private val log = LoggerFactory.getLogger(PointService::class.java)
+
+    private val userLock: ConcurrentHashMap<Long, ReentrantLock> = ConcurrentHashMap()
 
     fun chargeUserPoint(
         userId: Long, amount: Long
     ) : UserPointResponse {
+        log.debug("arrived charge " + System.nanoTime())
+        lockForUser(userId)
 
-        val userPoint = userPointTable.selectById(userId)
-        userPoint.chargePoint(amount)
+        try{
+            log.debug("start charge " + System.nanoTime())
+            val userPoint = userPointTable.selectById(userId)
+            userPoint.chargePoint(amount)
 
-        val chargedPoint = userPointTable.insertOrUpdate(userId, userPoint.point)
+            val chargedPoint = userPointTable.insertOrUpdate(userId, userPoint.point)
 
-        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, chargedPoint.updateMillis)
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, chargedPoint.updateMillis)
 
-        return UserPointResponse.of(chargedPoint)
+            log.debug("end charge " + System.nanoTime())
+
+            return UserPointResponse.of(chargedPoint)
+        }finally {
+            unlockForUser(userId)
+        }
+
+
     }
 
     fun useUserPoint(
         userId: Long, amount: Long
     ): UserPointResponse {
-        val userPoint = userPointTable.selectById(userId)
-        userPoint.usePoint(amount)
+        log.debug("arrived use" + System.currentTimeMillis())
 
-        val usedPoint = userPointTable.insertOrUpdate(userId, userPoint.point)
+        lockForUser(userId)
 
-        pointHistoryTable.insert(userId, amount, TransactionType.USE, usedPoint.updateMillis)
+        try{
+            log.debug("start use" + System.currentTimeMillis())
+            val userPoint = userPointTable.selectById(userId)
 
-        return UserPointResponse.of(usedPoint)
+            userPoint.usePoint(amount)
+
+            val usedPoint = userPointTable.insertOrUpdate(userId, userPoint.point)
+
+
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, usedPoint.updateMillis)
+
+            log.debug("end use" + System.currentTimeMillis())
+
+            return UserPointResponse.of(usedPoint)
+        }finally {
+            unlockForUser(userId)
+        }
+
     }
 
     fun getUserPointHistory(
         userId: Long
     ): List<PointHistoryResponse> {
-        return pointHistoryTable.selectAllByUserId(userId)
-            .map { PointHistoryResponse.of(it) }
+        log.debug("arrived history " + System.currentTimeMillis())
+
+        lockForUser(userId)
+        try{
+            log.debug("start history " + System.currentTimeMillis())
+            val histories = pointHistoryTable.selectAllByUserId(userId)
+            log.debug("end history " + System.currentTimeMillis())
+
+            return histories.map { PointHistoryResponse.of(it) }
+        }finally {
+            unlockForUser(userId)
+        }
     }
 
     fun getUserPoint(
         userId: Long
     ): UserPointResponse {
-        return UserPointResponse.of(userPointTable.selectById(userId))
+        log.debug("arrived get " + System.currentTimeMillis())
+
+        lockForUser(userId)
+        try{
+            log.debug("start get " + System.currentTimeMillis())
+            val userPoint = userPointTable.selectById(userId)
+            log.debug("end get " + System.currentTimeMillis())
+
+            return UserPointResponse.of(userPoint)
+        }finally {
+            unlockForUser(userId)
+        }
+
+    }
+
+    private fun lockForUser(key: Long) {
+        val lock = userLock.computeIfAbsent(key) { ReentrantLock(true) }
+        log.debug("Is this lock fair? {}", lock.isFair())
+        lock.lock()
+    }
+
+    private fun unlockForUser(key: Long) {
+        userLock[key]?.unlock()
     }
 }
--- src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
+++ src/test/kotlin/io/hhplus/tdd/service/PointServiceTest.kt
@@ -5,25 +5,35 @@ import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.PointHistory
 import io.hhplus.tdd.point.TransactionType
 import io.hhplus.tdd.point.UserPoint
+import io.hhplus.tdd.point.dto.response.PointHistoryResponse
+import io.hhplus.tdd.point.dto.response.UserPointResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PointServiceTest {
 
     private lateinit var userPointTable: UserPointTable
     private lateinit var pointHistoryTable: PointHistoryTable
     private lateinit var pointService: PointService
+    private lateinit var executorService: ExecutorService
 
     @BeforeEach
     fun setUp() {
         userPointTable = UserPointTable()
         pointHistoryTable = PointHistoryTable()
         pointService = PointService(pointHistoryTable, userPointTable)
+        executorService = Executors.newFixedThreadPool(5)
     }
 
     @Test
@@ -79,6 +89,18 @@ class PointServiceTest {
             .isInstanceOf(RuntimeException::class.java)
             .hasMessage("잔여 포인트가 부족합니다")
 
+    }
+
+    @Test
+    @DisplayName("새로운 유저의 포인트를 충전하지 않고 사용하려하면 잔액이 부족하다")
+    fun usePointWithoutInit() {
+        //given
+        val userId: Long = 10L;
+
+        //when then
+        assertThatThrownBy { pointService.useUserPoint(userId, 20L) }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessage("잔여 포인트가 부족합니다")
     }
 
     @Test
@@ -182,5 +204,281 @@ class PointServiceTest {
 
         //then
         assertThat(userPoint.point).isEqualTo(70)
+    }
+
+    @Test
+    @DisplayName("동시에 충전 요청을 할 경우 올바르게 값이 충전된다")
+    fun testConcurrentRequest() {
+        //given
+        val numberOfThread = 5
+        val startLatch = CountDownLatch(1)
+        val finishLatch = CountDownLatch(numberOfThread)
+        val userId = 10L;
+        userPointTable.insertOrUpdate(userId, 100L)
+
+        repeat(numberOfThread) {
+            executorService.submit {
+                try {
+                    startLatch.await()
+
+                    val response = pointService.chargeUserPoint(userId, 100L)
+
+                } finally {
+                    finishLatch.countDown()
+                }
+            }
+        }
+
+        startLatch.countDown()
+
+        executorService.shutdown()
+        executorService.awaitTermination(5, TimeUnit.SECONDS)
+
+        //when
+        val userPoint = pointService.getUserPoint(userId)
+        val userHistory = pointService.getUserPointHistory(userId)
+
+        //then
+        assertThat(userPoint.point).isEqualTo(600L)
+        assertThat(userHistory.size).isEqualTo(5)
+
+    }
+
+    @Test
+    @DisplayName("동시에 충전과 사용 요청을 할 경우 순서대로 올바르게 사용된다")
+    fun testConcurrentRequestWithUse() {
+        //given
+        val numberOfThread = 4
+        val startLatch = CountDownLatch(1)
+        val finishLatch = CountDownLatch(numberOfThread)
+        val userId = 10L;
+
+        executorService.submit {
+            try {
+                startLatch.await()
+
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(1)
+
+                val response = pointService.useUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(2)
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(3)
+                val response = pointService.useUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        startLatch.countDown()
+
+        executorService.shutdown()
+        executorService.awaitTermination(5, TimeUnit.SECONDS)
+
+        //when
+        val userPoint = pointService.getUserPoint(userId)
+        val userHistory = pointService.getUserPointHistory(userId)
+
+        //then
+        assertThat(userPoint.point).isEqualTo(0)
+        assertThat(userHistory.size).isEqualTo(4)
+        assertThat(userHistory.get(0).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(userHistory.get(1).type).isEqualTo(TransactionType.USE)
+        assertThat(userHistory.get(2).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(userHistory.get(3).type).isEqualTo(TransactionType.USE)
+    }
+
+    @Test
+    @DisplayName("동시의 여러 충전 요청 중 현재 포인트를 조회하면 올바르게 조회해야한다")
+    fun testGetPointWithConcurrentRequest() {
+        //given
+        val numberOfThread = 5
+        val startLatch = CountDownLatch(1)
+        val finishLatch = CountDownLatch(numberOfThread)
+        val userId = 10L;
+        lateinit var searchedPoint: UserPointResponse
+
+        executorService.submit {
+            try {
+                startLatch.await()
+
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(1)
+
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(2)
+
+                searchedPoint = pointService.getUserPoint(userId)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(3)
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(4)
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        startLatch.countDown()
+
+        executorService.shutdown()
+        executorService.awaitTermination(5, TimeUnit.SECONDS)
+
+        //when
+        val userHistory = pointService.getUserPointHistory(userId)
+
+        //then
+        assertThat(searchedPoint.point).isEqualTo(200)
+        assertThat(userHistory.size).isEqualTo(4)
+        assertThat(userHistory.get(0).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(userHistory.get(1).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(userHistory.get(2).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(userHistory.get(3).type).isEqualTo(TransactionType.CHARGE)
+    }
+
+    @Test
+    @DisplayName("동시의 여러 요청 중 이력을 조회하면 올바르게 조회해와야한다")
+    fun testGetHistoryWithConcurrentRequest() {
+        //given
+        val numberOfThread = 5
+        val startLatch = CountDownLatch(1)
+        val finishLatch = CountDownLatch(numberOfThread)
+        val userId = 10L;
+        lateinit var searchedHistory: List<PointHistoryResponse>
+
+        executorService.submit {
+            try {
+                startLatch.await()
+
+                val response = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(1)
+
+                val response = pointService.useUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(2)
+
+                val response  = pointService.chargeUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(3)
+                searchedHistory = pointService.getUserPointHistory(userId)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        executorService.submit {
+            try {
+                startLatch.await()
+                Thread.sleep(4)
+                val response = pointService.useUserPoint(userId, 100L)
+
+            } finally {
+                finishLatch.countDown()
+            }
+        }
+
+        startLatch.countDown()
+
+        executorService.shutdown()
+        executorService.awaitTermination(5, TimeUnit.SECONDS)
+
+        //when
+
+
+        //then
+        assertThat(searchedHistory.size).isEqualTo(3)
+        assertThat(searchedHistory.get(0).type).isEqualTo(TransactionType.CHARGE)
+        assertThat(searchedHistory.get(1).type).isEqualTo(TransactionType.USE)
+        assertThat(searchedHistory.get(2).type).isEqualTo(TransactionType.CHARGE)
     }
 }
